ca26ef8a70f78d9055aa57519b1e06ca545049f1
sashaafm <sashaafm@gmail.com>
2016-04-02 19:42:32 +0100

    Updated the version.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Gcloudex.Mixfile do
 
   def project do
     [app: :gcloudex,
-     version: "0.0.1",
+     version: "0.3.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
