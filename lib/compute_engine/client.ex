0f3b1435c9800bba14d06c28028bfef53dbb0019
Sasha Fonseca <sashaafm@gmail.com>
2016-04-20 19:43:58 +0100

    Fully implemented the Machine Types endpoint.

--- lib/compute_engine/client.ex
+++ lib/compute_engine/client.ex
@@ -189,8 +189,8 @@ defmodule GCloudex.ComputeEngine.Client do
   @doc """
   Retrieves aggregated list of instances.
   """
-  @spec get_aggregated_list() :: HTTPResponse.t
-  def get_aggregated_list do 
+  @spec get_aggregated_list_of_instances() :: HTTPResponse.t
+  def get_aggregated_list_of_instances do 
     Request.request(
       :get, 
       @no_zone_ep <> "/aggregated/instances",
@@ -202,8 +202,8 @@ defmodule GCloudex.ComputeEngine.Client do
   Retrieves aggregated list of instances according to the specified 
   'query_params'.
   """
-  @spec get_aggregated_list(map) :: HTTPResponse.t
-  def get_aggregated_list(query_params) do
+  @spec get_aggregated_list_of_instances(map) :: HTTPResponse.t
+  def get_aggregated_list_of_instances(query_params) do
     query = query_params |> URI.encode_query
 
     Request.request_query(
@@ -386,6 +386,55 @@ defmodule GCloudex.ComputeEngine.Client do
       [{"Content-Type", "application/json"}],
       body
       )
+  end
+
+  #####################
+  ### MACHINE TYPES ###
+  #####################
+
+  @doc """
+  Retrieves a list of machine types available in the specified 'zone'.
+  """
+  @spec list_machine_types(binary) :: HTTPResponse.t
+  def list_machine_types(zone) do 
+    Request.request(:get, @instance_ep <> "/#{zone}/machineTypes", [], "")
+  end
+
+  @doc """
+  Retrieves a list of machine types available in the specified 'zone' and 
+  that fit in the given 'query_params'.
+  """
+  @spec list_machine_types(binary, map) :: HTTPResponse.t
+  def list_machine_types(zone, query_params) do 
+    query = query_params |> URI.encode_query
+    Request.request_query(:get, @instance_ep <> "/#{zone}/machineTypes", [], "", "?" <> query)
+  end
+
+  @doc """
+  Returns the specified 'machine_type' in the given 'zone'.
+  """
+  @spec get_machine_type(binary, binary) :: HTTPResponse.t
+  def get_machine_type(zone, machine_type) do 
+    Request.request :get, @instance_ep <> "/#{zone}/machineTypes/#{machine_type}", [], ""
+  end
+
+  @doc """
+  Returns an aggregated list of machine types.
+  """
+  @spec get_aggregated_list_of_machine_types() :: HTTPResponse.t
+  def get_aggregated_list_of_machine_types do 
+    Request.request :get, @no_zone_ep <> "/aggregated/machineTypes", [], ""
+  end
+
+  @doc """
+  Returns an aggragated list of machine types following the specified 
+  'query_params'.
+  """
+  @spec get_aggregated_list_of_machine_types(map) :: HTTPResponse.t
+  def get_aggregated_list_of_machine_types(query_params) do 
+    query = query_params |> URI.encode_query
+
+    Request.request_query :get, @no_zone_ep <> "/aggregated/machineTypes", [], "", "?" <> query
   end
 
   ########################
