62b1b816b738ec8cd7fd66973f00f3ec14f39075
Sasha Fonseca <sashaafm@gmail.com>
2016-05-05 15:57:15 +0100

    Still setting up Travis CI.

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,11 @@
 use Mix.Config
 
 config :goth, 
-  json: "config/creds.json" |> Path.expand |> File.read!
+  json: if File.exists?("config/creds.json") do 
+          "config/creds.json" |> Path.expand |> File.read!
+        else
+          "config/creds_test.json" |> Path.expand |> File.read!
+        end
 
 config :logger,
   level: :debug
