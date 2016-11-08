bd05d1e26eae13405c7342545a5240ca3e9113b8
Michael Bearne <michael@bearne.com>
2016-11-08 13:37:51 +0000

    update httpoison and goth dependencies

--- mix.exs
+++ mix.exs
@@ -35,8 +35,8 @@ defmodule GCloudex.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.8.3"},
-      {:goth,      "~> 0.1.2"},
+      {:httpoison, "~> 0.9.2"},
+      {:goth,      "~> 0.2.1"},
       {:poison,    "~> 1.2 or ~> 2.1"},
       {:credo,     "~> 0.3.13", only: [:dev, :test]},
       {:ex_doc,    ">= 0.11.0", only: [:dev]},
